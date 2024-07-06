a9888e9f85ca8b0cd55304759ce1db5e697c5b2e
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-06 21:03:22 +0530

    Add hover colour change

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.querySelector(".container");
 
-document.addEventListener("DOMContentLoaded", () => {
-    for (let divRow = 1; divRow < 17; divRow++) {
+document.addEventListener("DOMContentLoaded", createGrid(16));
+
+container.addEventListener("mouseover", (event) => recolour(event));
+
+function createGrid(squareCount) {
+    for (let divRow = 0; divRow < squareCount; divRow++) {
         const divRow = document.createElement("div");
         divRow.setAttribute("class", "gridrow");
 
-        for (let divColumn = 1; divColumn < 17; divColumn++) {
+        for (let divColumn = 0; divColumn < squareCount; divColumn++) {
             const div = document.createElement("div");
             div.setAttribute("class", "gridbox");
             divRow.appendChild(div);
@@ -13,4 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         container.appendChild(divRow);
     }
-});
+}
+
+function recolour(ev) {
+    if (ev.target.getAttribute("class") != "container") {
+        ev.target.style["background-color"] = "aquamarine";
+    }
+}
